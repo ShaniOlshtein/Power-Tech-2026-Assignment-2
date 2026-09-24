b2758d1713f991f7c4e7a92bc1e8b0bfaf59b51c
ShaniOlshtein <s0533173756@gmail.com>
2026-09-24 22:14:51 +0300

    Add farewell function
    Added farewell function to return a goodbye message.

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ def greet(name):
     """Return a greeting for the given name."""
     return f"{GREETING}, {name}!"
 
+def farewell(name):
+    &quot;&quot;&quot;Return a farewell message for the given name.&quot;&quot;&quot;
+    return f&quot;Goodbye, {name}!&quot;
 
 def app_info():
     """Return basic information about the app."""
